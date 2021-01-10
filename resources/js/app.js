2bdd8edd09419abd5e8f1d9b4fe204541405749c
jeanpdt <rodrigo.santospdt@gmail.com>
2021-01-10 14:50:32 -0400

    added authentication part 2

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,32 @@
 /* eslint-env node */
 require('./bootstrap');
 
-import Vue from 'vue';
-import VueRouter from 'vue-router';
 
-import router from './Router/index';
+import 'es6-promise/auto';
+import axios from 'axios';
+import Vue from 'vue';
+import VueAuth from '@websanova/vue-auth';
+import VueAxios from 'vue-axios';
+import VueRouter from 'vue-router';
 import store from './Store/index';
 import App from './App';
+import auth from './auth';
+import router from './Router/index';
 
+
+
+window.Vue = Vue;
+
+Vue.router = router;
 Vue.use(VueRouter);
+
+
+// Set Vue authentication
+Vue.use(VueAxios, axios);
+axios.defaults.baseURL = `${process.env.MIX_APP_URL}/api`;
+
+Vue.use(VueAuth, auth);
+
 
 // eslint-disable-next-line no-unused-vars
 const app = new Vue({
